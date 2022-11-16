5afbb51a0e20a8ffc2d2fe581a70664c41f2d3c5
Abeshu <gwabeshu@CougarNet.UH.EDU>
2022-11-16 17:32:50 -0600

    Update test_calibration_managed.py

--- xanthos-wm/xanthos/tests/test_calibration_managed.py
+++ xanthos-wm/xanthos/tests/test_calibration_managed.py
@@ -2,7 +2,6 @@ import os
 import glob
 import pkg_resources
 import unittest
-
 import pandas as pd
 
 from xanthos import Xanthos
@@ -12,9 +11,9 @@ class TestCalibManaged(unittest.TestCase):
 
     """Test that the default outputs do not change."""
 
-    DEFAULT_CONFIG_FILE = pkg_resources.resource_filename('xanthos-wm/xanthos', 'tests/data/inputs/pm_abcd_mrtm_managed.ini')
-    EXAMPLE_DIR = pkg_resources.resource_filename('xanthos-wm/xanthos', 'test/data')
-    DEFAULT_OUTPUTS_DIR = pkg_resources.resource_filename('xanthos-wm/xanthos', 'test/data/outputs/pm_abcd_mrtm_watch_1971_2001')
+    DEFAULT_CONFIG_FILE = pkg_resources.resource_filename('xanthos', 'tests/data/inputs/pm_abcd_mrtm_managed.ini')
+    EXAMPLE_DIR = pkg_resources.resource_filename('xanthos', 'test/data')
+    DEFAULT_OUTPUTS_DIR = pkg_resources.resource_filename('xanthos', 'test/data/outputs/pm_abcd_mrtm_watch_1971_2001')
 
     def test_outputs(self):
         """Test that Xanthos produces correct outputs for the default configuration."""
